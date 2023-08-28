88d73dad593eaec99e9e1dfe10f953795fd6310f
AproGGG <143408932+AproGGG@users.noreply.github.com>
2023-08-28 18:24:29 +0400

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post('/upload', (req, res) => {
     let file = req.files.image;
     let date = new Date();
     let imagename = date.getDate() + date.getTime() + file.name;
-    let path = 'public/uploads/' + imagename;
+    let path = '/uploads/' + imagename;
 
     file.mv(path, (err, result) => {
         if(err){
